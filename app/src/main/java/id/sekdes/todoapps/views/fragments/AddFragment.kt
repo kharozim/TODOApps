61417fcbbbe35b90a989e477bc82548ece6d6583
kharozim <kharozim06@gmail.com>
2020-12-10 09:01:36 +0700

    add datetime
    Signed-off-by: kharozim <kharozim06@gmail.com>

--- app/src/main/java/id/sekdes/todoapps/views/fragments/AddFragment.kt
+++ app/src/main/java/id/sekdes/todoapps/views/fragments/AddFragment.kt
@@ -6,14 +6,12 @@ import android.os.Build
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.Toast
 import androidx.annotation.RequiresApi
-import id.sekdes.todoapps.views.contracts.TodoAddContract
+import androidx.fragment.app.Fragment
 import id.sekdes.todoapps.databinding.FragmentAddBinding
 import id.sekdes.todoapps.models.TodoModel
 import id.sekdes.todoapps.presenter.TodoAddPresenter
@@ -21,10 +19,10 @@ import id.sekdes.todoapps.repository.TodoLocalRepository
 import id.sekdes.todoapps.repository.locale.TodoLocalRepositoryImpl
 import id.sekdes.todoapps.repository.locale.daos.TodoDao
 import id.sekdes.todoapps.repository.locale.databases.LocaleDatabase
+import id.sekdes.todoapps.views.contracts.TodoAddContract
 import id.sekdes.todoapps.views.utils.Constant
 import id.sekdes.todoapps.views.utils.DateUtil
 import java.time.LocalDateTime
-import java.time.LocalTime
 import java.util.*
 
 
@@ -35,8 +33,7 @@ class AddFragment : Fragment(), TodoAddContract.View {
     private val repository: TodoLocalRepository by lazy { TodoLocalRepositoryImpl(dao) }
     private val presenter: TodoAddContract.Presenter by lazy { TodoAddPresenter(this, repository) }
 
-//    private lateinit var pickedDateTime: LocalDateTime
-    private lateinit var pickedDateTime: LocalTime
+    private lateinit var pickedDateTime: LocalDateTime
 
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -55,19 +52,24 @@ class AddFragment : Fragment(), TodoAddContract.View {
     private fun setView() {
         binding.apply {
             btTime.setOnClickListener { openDateTimePicker() }
-            btTime.addTextChangedListener(object : TextWatcher{
-                override fun beforeTextChanged(s: CharSequence?,start: Int,count: Int,after: Int) {
-                }
-
-                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                    TODO("Not yet implemented")
-                }
-
-                override fun afterTextChanged(s: Editable?) {
-                    btTime.error = null
-                }
-
-            })
+//            btTime.addTextChangedListener(object : TextWatcher {
+//                override fun beforeTextChanged(
+//                    s: CharSequence?,
+//                    start: Int,
+//                    count: Int,
+//                    after: Int
+//                ) {
+//                }
+//
+//                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+//                    TODO("Not yet implemented")
+//                }
+//
+//                override fun afterTextChanged(s: Editable?) {
+//                    btTime.error = null
+//                }
+//
+//            })
 
             btSave.setOnClickListener {
                 presenter.insertTodo(
@@ -92,32 +94,31 @@ class AddFragment : Fragment(), TodoAddContract.View {
             val startHour = instance.get(Calendar.HOUR_OF_DAY)
             val startMinute = instance.get(Calendar.MINUTE)
 
-//            val datePickerDialog = DatePickerDialog(requireContext(), { _, year, month, day ->
-               val timePickerDialog =  TimePickerDialog(requireContext(), { _, hour, minute ->
-                    pickedDateTime =
-//                        LocalDateTime.of(
-//                            year,
-//                            month + 1,
-//                            day,
-                        LocalTime.of(
-                            hour,
-                            minute
-                        )
+            val datePickerDialog = DatePickerDialog(requireContext(), { _, year, month, day ->
+                TimePickerDialog(requireContext(), { _, hour, minute ->
+                pickedDateTime =
+                    LocalDateTime.of(
+                        year,
+                        month + 1,
+                        day,
+                        hour,
+                        minute
+                    )
 
-                    if (pickedDateTime.hour < startHour) {
-                        showMessage("You are not allowed to specify an earlier time!")
-                        btTime.error = "Select valid time"
-                        btTime.text = Constant.SELECT_DATE
-                    } else {
-                        btTime.text = pickedDateTime.format(DateUtil.timeFormat)
-                    }
-                }, startHour, startMinute, true).show()
-//            }, startYear, startMonth, startDay)
+                if (pickedDateTime.dayOfMonth == startDay && pickedDateTime.hour < startHour) {
+                    showMessage("You are not allowed to specify an earlier time!")
+                    btTime.error = "Select valid time"
+                    btTime.text = Constant.SELECT_DATE
+                } else {
+                    btTime.text = pickedDateTime.format(DateUtil.dateTimeFormat)
+                }
+            }, startHour, startMinute, true).show()
+            }, startYear, startMonth, startDay)
 
-//            datePickerDialog.datePicker
-//                .minDate = System.currentTimeMillis()
+            datePickerDialog.datePicker
+                .minDate = System.currentTimeMillis()
 
-//            datePickerDialog.show()
+            datePickerDialog.show()
         }
     }
 
