375f6b38d0139b90fec4a818bd5a7eeaeca10b00
PC-4 <kharozim06@gmail.com>
2020-12-12 12:38:39 +0700

    image detail
    Signed-off-by: PC-4 <kharozim06@gmail.com>

--- app/src/main/java/id/sekdes/todoapps/views/fragments/DetailFragment.kt
+++ app/src/main/java/id/sekdes/todoapps/views/fragments/DetailFragment.kt
@@ -1,10 +1,12 @@
 package id.sekdes.todoapps.views.fragments
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import id.sekdes.todoapps.views.contracts.TodoEditContract
@@ -15,10 +17,15 @@ import id.sekdes.todoapps.repository.TodoLocalRepository
 import id.sekdes.todoapps.repository.locale.TodoLocalRepositoryImpl
 import id.sekdes.todoapps.repository.locale.daos.TodoDao
 import id.sekdes.todoapps.repository.locale.databases.LocaleDatabase
+import id.sekdes.todoapps.views.adapters.ImageAdapter
+import id.sekdes.todoapps.views.adapters.ImageDetailAdapter
 
-class DetailFragment : Fragment(), TodoEditContract.View {
+
+class DetailFragment : Fragment(), ImageAdapter.ImageListener, TodoEditContract.View {
 
     private val args by navArgs<DetailFragmentArgs>()
+    private val imageAdapter by lazy { ImageAdapter(requireContext(), this) }
+//    private val imageAdapter by lazy {ImageDetailAdapter(requireContext())  }
     private lateinit var binding: FragmentDetailBinding
     private val dao: TodoDao by lazy { LocaleDatabase.getDatabase(requireContext()).dao() }
     private val repository: TodoLocalRepository by lazy { TodoLocalRepositoryImpl(dao) }
@@ -30,10 +37,14 @@ class DetailFragment : Fragment(), TodoEditContract.View {
     ): View? {
         binding = FragmentDetailBinding.inflate(inflater, container, false).apply {
 
-//            tieName.setText(args.todo.title)
-//            tieDescription.setText(args.todo.description)
+            rvImage.adapter = imageAdapter
+
             etTitle.setText(args.todo.title)
-            btTime.text = args.todo.dueTime
+            tvTime.setText(args.todo.dueTime)
+            args.todo.images?.asSequence()?.map { Uri.parse(it) }
+                ?.toMutableList()?.let { imageAdapter.setData(it) }
+
+
 
 
             btUpdate.setOnClickListener {
@@ -41,7 +52,7 @@ class DetailFragment : Fragment(), TodoEditContract.View {
                     TodoModel(
                         args.todo.id,
                         title = etTitle.text.toString(),
-                        dueTime = btTime.text.toString()
+                        images = args.todo.images
                     )
                 )
             }
@@ -68,4 +79,14 @@ class DetailFragment : Fragment(), TodoEditContract.View {
     override fun onEmptyTodo(state: Boolean) {
         TODO("Not yet implemented")
     }
+
+    override fun onClick(uri: Uri) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onDelete(uri: Uri) {
+        TODO("Not yet implemented")
+    }
+
+
 }
